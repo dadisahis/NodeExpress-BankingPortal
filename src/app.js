59acf3d1d71757f51ad317bb181607b1efd1f305
Adithya Saish <adiwithmj@gmail.com>
2022-06-14 17:12:46 +0530

    added logic to handle payment and money transfer

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const accountData = fs.readFileSync(
   path.join(__dirname, "/json/accounts.json"),
   (encoding = "utf8")
 );
+app.use(express.urlencoded({ extended: true }));
 const accounts = JSON.parse(accountData);
 
 const userData = fs.readFileSync(
@@ -35,6 +36,40 @@ app.get("/checking", (req, res) => {
 
 app.get("/profile", (req, res) => {
   res.render("profile", { user: users[0] });
+});
+
+app.get("/transfer", (req, res) => res.render("transfer"));
+
+app.post("/transfer", (req, res) => {
+  accounts[req.body.from].balance -= req.body.amount;
+  accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+  let accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(
+    path.join(__dirname, "/json/accounts.json"),
+    accountsJSON,
+    "utf8"
+  );
+  res.render("transfer", { message: "Transfer Completed" });
+});
+
+app.get("/payment", (req, res) => {
+  res.render("payment", { account: accounts.credit });
+});
+
+app.post("/payment", (req, res) => {
+  accounts.credit.balance -= parseInt(req.body.amount);
+  accounts.credit.available += parseInt(req.body.amount);
+  let accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(
+    path.join(__dirname, "/json/accounts.json"),
+    accountsJSON,
+    "utf8"
+  );
+
+  res.render("payment", {
+    message: "Payment Successful",
+    account: accounts.credit,
+  });
 });
 
 app.listen(3000, () => {
